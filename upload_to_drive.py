5b0226fc8f53be3158c242a71a7a60a43a6a92fc
NMTEngineering <officialnmtengineering@gmail.com>
2025-09-14 20:39:06 +0530

    updated script

--- upload_to_drive.py
+++ upload_to_drive.py
@@ -1,29 +1,22 @@
 from pydrive2.auth import GoogleAuth
 from pydrive2.drive import GoogleDrive
-from oauth2client.service_account import ServiceAccountCredentials
 import os
 
 # Save credentials from GitHub secret
 with open("gdrive_key.json", "w") as f:
     f.write(os.environ["GDRIVE_KEY"])
 
-# Authenticate
+# Authenticate using pydrive2's LoadServiceConfigFile method
 gauth = GoogleAuth()
-gauth.credentials = ServiceAccountCredentials.from_json_keyfile_name(
-    "gdrive_key.json",
-    ["https://www.googleapis.com/auth/drive"]
-)
+gauth.LoadServiceConfigFile("gdrive_key.json")
 drive = GoogleDrive(gauth)
 
-# Set file and folder ID (Shared Drive folder)
 file_name = "yellowpages_hammer_unions.csv"
 folder_id = "1QyoY1rs9Mv1yHa9-XDXGPZ4VK8ETduY3"
 
-# Upload with shared drive support
 gfile = drive.CreateFile({
     "title": file_name,
-    "parents": [{"id": folder_id}],
-    "supportsAllDrives": True
+    "parents": [{"id": folder_id}]
 })
 gfile.SetContentFile(file_name)
 gfile.Upload(param={"supportsAllDrives": True})
